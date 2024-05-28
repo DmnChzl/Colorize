05f104564e126552dd87da1dac2f349e6107634f
Damien Chazoule <damien.chazoule@gmail.com>
2024-05-28 22:07:36 +0200

    Feat: Alpha Support (alphaToHex)

--- README.md
+++ README.md
@@ -1,12 +1,147 @@
 # Colorize
 
-> 0.1.0
+> 0.2.0
 
-Utilities for colors and luminance
+Utilities for colors and luminance, published on [JSR](https://jsr.io).
+
+## Installation
+
+First of all, you need to install the package:
+
+```
+npx jsr add @dmnchzl/colorize
+```
+
+<details>
+  <summary>Equivalent with Yarn</summary>
+  
+  ```
+  yarn dlx jsr add @dmnchzl/colorize
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with pNPM</summary>
+  
+  ```
+  pnpm dlx jsr add @dmnchzl/colorize
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with Deno</summary>
+  
+  ```
+  deno add @dmnchzl/colorize
+  ```
+
+</details>
+
+<details>
+  <summary>Equivalent with Bun</summary>
+  
+  ```
+  bunx jsr add @dmnchzl/colorize
+  ```
+
+</details>
 
 ## Utilities
 
-Lorem ipsum dolor sit amet...
+Below, the list of all available utilities:
+
+**alphaToHex**
+
+> Convert alpha value into HEX value
+
+```js
+import { alphaToHex } from '@dmnchzl/colorize';
+
+console.log(alphaToHex(0)); // 00
+console.log(alphaToHex(0.25)); // 40
+console.log(alphaToHex(0.5)); // 80
+console.log(alphaToHex(0.75)); // bf
+console.log(alphaToHex(1)); // ff
+```
+
+**hexToRgb**
+
+> Extract RGB(A) values from HEX color
+
+```js
+import { hexToRgb } from '@dmnchzl/colorize';
+
+console.log(hexToRgb('#f5f5f5')); // [245, 245, 245]
+console.log(hexToRgb('#f5f5f580')); // [245, 245, 245, 0.5]
+```
+
+**hexToRgbStr**
+
+> Convert HEX color into RGB color
+
+```js
+import { hexToRgbStr } from '@dmnchzl/colorize';
+
+console.log(hexToRgbStr('#f5f5f5')); // rgb(245, 245, 245)
+console.log(hexToRgbStr('#f5f5f580')); // rgba(245, 245, 245, 0.5)
+```
+
+**rgbToHex**
+
+> Retrieve HEX color from RGB values
+
+```js
+import { rgbToHex } from '@dmnchzl/colorize';
+
+console.log(rgbToHex(245, 245, 245)); // #f5f5f5
+console.log(rgbToHex(245, 245, 245, 0.5)); // #f5f5f580
+```
+
+**rgbStrToHex**
+
+> Convert RGB color into HEX color
+
+```js
+import { rgbStrToHex } from '@dmnchzl/colorize';
+
+console.log(rgbStrToHex('rgb(245, 245, 245)')); // #f5f5f5
+console.log(rgbStrToHex('rgba(245, 245, 245, 0.5)')); // #f5f5f580
+```
+
+**isLightOrDarkColor**
+
+> Check if the color is 'light' or 'dark' based on threshold
+
+```js
+import { isLightOrDarkColor } from '@dmnchzl/colorize';
+
+console.log(isLightOrDarkColor('#f5f5f5')); // light
+console.log(isLightOrDarkColor('rgba(245, 245, 245, 0.5)')); // light
+```
+
+**isLightColor**
+
+> Check if the color is 'light' based on threshold
+
+```js
+import { isLightColor } from '@dmnchzl/colorize';
+
+console.log(isLightColor('#f5f5f5')); // true
+console.log(isLightColor('rgba(245, 245, 245, 0.5)')); // true
+```
+
+**isDarkColor**
+
+> Check if the color is 'dark' based on threshold
+
+```js
+import { isDarkColor } from '@dmnchzl/colorize';
+
+console.log(isDarkColor('#f5f5f5')); // false
+console.log(isDarkColor('rgba(245, 245, 245, 0.5)')); // false
+```
 
 ## License
 
--- index.ts
+++ index.ts
@@ -1,8 +1,32 @@
 /**
- * Extract RGB values from HEX color
+ * Convert alpha value into HEX value
+ *
+ * @param {number} alpha 0 < 1
+ * @returns {string} #alpha
+ */
+export function alphaToHex(alpha: number): string {
+  if (alpha < 0 || alpha > 1) {
+    throw new Error('Alpha Value Must Be Between 0-1');
+  }
+
+  // Scale alpha to the range 0-255, and round to the nearest integer
+  return Math.round(alpha * 255)
+    .toString(16)
+    .padStart(2, '0');
+}
+
+/**
+ * This is a capturing group that matches:
+ * starts with (`^`) and ends with (`$`) exactly two characters (`{2}`),
+ * that are either digits (`0-9`), lowercase letters (`a-f`), or uppercase letters (`A-F`)
+ */
+const TWO_DIGIT_HEX_RE = /^([0-9a-fA-F]{2})$/;
+
+/**
+ * Extract RGB(A) values from HEX color
  *
  * @param {string} hex #color
- * @returns {number[]} RGB values
+ * @returns {number[]} RGB(A) values
  */
 export function hexToRgb(hex: string): number[] {
   // Remove the hash (at the start)
@@ -16,10 +40,31 @@ export function hexToRgb(hex: string): number[] {
       .join('');
   }
 
+  const rStr = hex.substring(0, 2);
+  const gStr = hex.substring(2, 4);
+  const bStr = hex.substring(4, 6);
+
+  // Ensure HEX values are valid two-character strings
+  if (!TWO_DIGIT_HEX_RE.test(rStr) || !TWO_DIGIT_HEX_RE.test(gStr) || !TWO_DIGIT_HEX_RE.test(bStr)) {
+    throw new Error('Incorrect HEX Color Format');
+  }
+
   // Convert to RGB values
-  const r = parseInt(hex.substring(0, 2), 16);
-  const g = parseInt(hex.substring(2, 4), 16);
-  const b = parseInt(hex.substring(4, 6), 16);
+  const r = parseInt(rStr, 16);
+  const g = parseInt(gStr, 16);
+  const b = parseInt(bStr, 16);
+
+  const aStr = hex.substring(6, 8);
+
+  // Ensure the HEX value is a valid two-character string
+  if (TWO_DIGIT_HEX_RE.test(aStr)) {
+    const value = parseInt(aStr, 16);
+
+    // Scale the value to the range 0-1, and round to the second decimal
+    const a = Math.round((value / 255) * 100) / 100;
+
+    return [r, g, b, a];
+  }
 
   return [r, g, b];
 }
@@ -31,8 +76,11 @@ export function hexToRgb(hex: string): number[] {
  * @returns {string} rbg(color)
  */
 export function hexToRgbStr(hex: string): string {
-  const [r, g, b] = hexToRgb(hex);
-  return `rgb(${r}, ${g}, ${b})`;
+  const [r, g, b, a] = hexToRgb(hex);
+  const rgb = `${r}, ${g}, ${b}`;
+
+  if (a) return `rgba(${rgb}, ${a})`;
+  return `rgb(${rgb})`;
 }
 
 /**
@@ -41,24 +89,27 @@ export function hexToRgbStr(hex: string): string {
  * @param {number} r red value
  * @param {number} g green value
  * @param {number} b blue value
+ * @param {number} a alpha value
  * @returns {string} #color
  */
-export function rgbToHex(r: number, g: number, b: number): string {
+export function rgbToHex(r: number, g: number, b: number, a?: number): string {
   /**
    * Convert each value to a hexadecimal,
    * then ensure it's 2 chars long
    */
   const toHex = (value: number): string => value.toString(16).padStart(2, '0');
 
-  return `#${[r, g, b].map(toHex).join('')}`;
+  let hex = `#${[r, g, b].map(toHex).join('')}`;
+  if (a) hex += alphaToHex(a);
+  return hex;
 }
 
 function rgbFromStr(rgbStr: string): number[] {
   // Extract the numbers from the RGB string
-  const values = rgbStr.match(/\d+/g);
+  const values = rgbStr.match(/\d+\.?\d+/g);
 
-  if (!values || values.length !== 3) {
-    throw new Error('Invalid RGB Color String');
+  if (!values || values.length < 3 || values.length > 4) {
+    throw new Error('Invalid RGB(A) Color String');
   }
 
   // Convert each value to an integer
@@ -72,8 +123,8 @@ function rgbFromStr(rgbStr: string): number[] {
  * @returns {string} #color
  */
 export function rgbStrToHex(rgbStr: string): string {
-  const [r, g, b] = rgbFromStr(rgbStr);
-  return rgbToHex(r, g, b);
+  const [r, g, b, a] = rgbFromStr(rgbStr);
+  return rgbToHex(r, g, b, a);
 }
 
 /** Convert RGB values to relative luminance */
@@ -107,10 +158,12 @@ export function isLightOrDarkColor(color: string): string {
   return luminance > 0.5 ? 'light' : 'dark'; // luminance <= 0.5 === dark
 }
 
+/** Check if the color is 'light' based on threshold */
 export function isLightColor(color: string): boolean {
   return isLightOrDarkColor(color) === 'light';
 }
 
+/** Check if the color is 'dark' based on threshold */
 export function isDarkColor(color: string): boolean {
   return isLightOrDarkColor(color) === 'dark';
 }
